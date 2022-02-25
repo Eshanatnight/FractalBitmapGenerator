cd507d64a40e6e45d864a0e9aab7ce711f84e092
Eshan Chatterjee <eshan2001221@gmail.com>
2022-02-25 17:17:50 +0530

    Refactor Files

---
+++ src/include/BitmapFileHeader.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include<cstdint>
+
+// using namespace std;
+
+namespace Bitmapping
+{
+
+	#pragma pack(push, 2)
+
+	struct BitmapFileHeader
+	{
+		char header[2] { 'B', 'M' };
+		int32_t fileSize;
+		int32_t reserved{0};
+		int32_t dataOffset;
+	};
+
+	#pragma pack(pop)
+
+}
